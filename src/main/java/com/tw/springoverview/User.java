b008581d9d6844820ade13e3c3ebfa7cc14a1901
gitqh <quhang19881208@163.com>
2022-07-15 11:00:18 +0800

    feat: import lombok and use it

--- src/main/java/com/tw/springoverview/User.java
+++ src/main/java/com/tw/springoverview/User.java
@@ -1,34 +1,18 @@
 package com.tw.springoverview;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author hqu
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
 
     private long id;
     private String username;
 
-    public User() {
-    }
-
-    public User(long id, String username) {
-        this.id = id;
-        this.username = username;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
